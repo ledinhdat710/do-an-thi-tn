b73ff90b0a9cec453e5edd51b306a0a66228e509
Quyen Pham <phamquyendh002@gmail.com>
2023-11-12 15:22:25 +0700

    change name db

--- config/connect.php
+++ config/connect.php
@@ -2,7 +2,7 @@
 if(!defined('TEMPLATE')){
 	die('Bạn không có quyền truy cập vào file này !');
 }
-$conn = mysqli_connect('localhost', 'root', '', 'do_an_my_love');
+$conn = mysqli_connect('localhost', 'root', '', 'do_an');
 if($conn){
 	mysqli_query($conn, "SET NAMES 'utf8'");
 }
---
+++ do_an_my_love.sql
@@ -0,0 +1,223 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 12, 2023 lúc 08:53 AM
+-- Phiên bản máy phục vụ: 10.4.28-MariaDB
+-- Phiên bản PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `do_an_my_love`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `category`
+--
+
+CREATE TABLE `category` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `category`
+--
+
+INSERT INTO `category` (`cat_id`, `cat_name`) VALUES
+(2, 'Đề thi'),
+(3, 'Liên hệ'),
+(1, 'Trang chủ');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `comment`
+--
+
+CREATE TABLE `comment` (
+  `comm_id` int(11) NOT NULL,
+  `prd_id` int(11) NOT NULL,
+  `comm_name` varchar(255) NOT NULL,
+  `comm_mail` varchar(255) NOT NULL,
+  `comm_date` datetime NOT NULL,
+  `comm_details` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `comment`
+--
+
+INSERT INTO `comment` (`comm_id`, `prd_id`, `comm_name`, `comm_mail`, `comm_date`, `comm_details`) VALUES
+(1, 1, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(2, 2, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(3, 3, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(4, 4, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(5, 5, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(6, 6, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(7, 7, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(8, 8, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(9, 9, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(10, 10, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(11, 11, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(12, 12, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(13, 13, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(14, 14, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(15, 15, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(16, 16, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(17, 17, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(18, 18, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(19, 19, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(20, 20, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(21, 21, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(22, 22, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(23, 23, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(24, 24, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(25, 25, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(26, 26, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(27, 27, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(28, 28, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(29, 29, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời'),
+(30, 30, 'Nguyễn Văn A', 'nguyenvana@gmail.com', '2018-12-12 20:59:56', 'Đây thực sự là một sản phẩm tuyệt vời');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `product`
+--
+
+CREATE TABLE `product` (
+  `prd_id` int(11) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `prd_name` varchar(255) NOT NULL,
+  `prd_image` varchar(255) NOT NULL,
+  `prd_price` int(11) UNSIGNED NOT NULL,
+  `prd_warranty` varchar(255) NOT NULL,
+  `prd_accessories` varchar(255) NOT NULL,
+  `prd_new` varchar(255) NOT NULL,
+  `prd_promotion` varchar(255) NOT NULL,
+  `prd_status` int(1) NOT NULL,
+  `prd_featured` int(1) NOT NULL,
+  `prd_details` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `product`
+--
+
+INSERT INTO `product` (`prd_id`, `cat_id`, `prd_name`, `prd_image`, `prd_price`, `prd_warranty`, `prd_accessories`, `prd_new`, `prd_promotion`, `prd_status`, `prd_featured`, `prd_details`) VALUES
+(1, 1, 'Đề thi toán 7', 'tải xuống.jpg', 10000000, '12 Tháng', 'Hộp, sách, sạc, cáp, tai nghe', 'Máy Mới 100%', 'Dán Màn Hình 4D', 1, 1, '<p>Sản phẩm n&agrave;y ch&uacute;ng t&ocirc;i đang cập nhật nội dung chi tiết, c&aacute;c bạn c&oacute; thể qua trực tiếp cửa h&agrave;ng để xem sản phẩm, v&igrave; h&agrave;ng ch&uacute;ng t&ocirc;i lu&ocirc;n c&oacute; sẵn.</p>\r\n'),
+(2, 1, 'Đề thi toán 6', 'tải xuống.jpg', 10000000, '12 Tháng', 'Hộp, sách, sạc, cáp, tai nghe', 'Máy Mới 100%', 'Dán Màn Hình 4D', 1, 1, '<p>Sản phẩm n&agrave;y ch&uacute;ng t&ocirc;i đang cập nhật nội dung chi tiết, c&aacute;c bạn c&oacute; thể qua trực tiếp cửa h&agrave;ng để xem sản phẩm, v&igrave; h&agrave;ng ch&uacute;ng t&ocirc;i lu&ocirc;n c&oacute; sẵn.</p>\r\n'),
+(3, 1, 'Đề thi toán 5', 'tải xuống.jpg', 10000000, '12 Tháng', 'Hộp, sách, sạc, cáp, tai nghe', 'Máy Mới 100%', 'Dán Màn Hình 4D', 1, 0, '<p>Sản phẩm n&agrave;y ch&uacute;ng t&ocirc;i đang cập nhật nội dung chi tiết, c&aacute;c bạn c&oacute; thể qua trực tiếp cửa h&agrave;ng để xem sản phẩm, v&igrave; h&agrave;ng ch&uacute;ng t&ocirc;i lu&ocirc;n c&oacute; sẵn.</p>\r\n'),
+(4, 1, 'Đề thi toán 4', 'tải xuống.jpg', 10000000, '12 Tháng', 'Hộp, sách, sạc, cáp, tai nghe', 'Máy Mới 100%', 'Dán Màn Hình 4D', 1, 0, '<p>Sản phẩm n&agrave;y ch&uacute;ng t&ocirc;i đang cập nhật nội dung chi tiết, c&aacute;c bạn c&oacute; thể qua trực tiếp cửa h&agrave;ng để xem sản phẩm, v&igrave; h&agrave;ng ch&uacute;ng t&ocirc;i lu&ocirc;n c&oacute; sẵn.</p>\r\n'),
+(5, 1, 'Đề thi toán 3', 'tải xuống.jpg', 10000000, '12 Tháng', 'Hộp, sách, sạc, cáp, tai nghe', 'Máy Mới 100%', 'Dán Màn Hình 4D', 0, 0, '<p>Sản phẩm n&agrave;y ch&uacute;ng t&ocirc;i đang cập nhật nội dung chi tiết, c&aacute;c bạn c&oacute; thể qua trực tiếp cửa h&agrave;ng để xem sản phẩm, v&igrave; h&agrave;ng ch&uacute;ng t&ocirc;i lu&ocirc;n c&oacute; sẵn.</p>\r\n'),
+(6, 2, 'Đề thi toán 12', 'tải xuống.jpg', 10000000, '12 Tháng', 'Hộp, sách, sạc, cáp, tai nghe', 'Máy Mới 100%', 'Dán Màn Hình 4D', 1, 1, '<p>Sản phẩm n&agrave;y ch&uacute;ng t&ocirc;i đang cập nhật nội dung chi tiết, c&aacute;c bạn c&oacute; thể qua trực tiếp cửa h&agrave;ng để xem sản phẩm, v&igrave; h&agrave;ng ch&uacute;ng t&ocirc;i lu&ocirc;n c&oacute; sẵn.</p>\r\n'),
+(7, 2, 'Đề thi toán 11', 'tải xuống.jpg', 10000000, '12 Tháng', 'Hộp, sách, sạc, cáp, tai nghe', 'Máy Mới 100%', 'Dán Màn Hình 4D', 1, 1, '<p>Sản phẩm n&agrave;y ch&uacute;ng t&ocirc;i đang cập nhật nội dung chi tiết, c&aacute;c bạn c&oacute; thể qua trực tiếp cửa h&agrave;ng để xem sản phẩm, v&igrave; h&agrave;ng ch&uacute;ng t&ocirc;i lu&ocirc;n c&oacute; sẵn.</p>\r\n'),
+(8, 2, 'Đề thi toán 10', 'tải xuống.jpg', 10000000, '12 Tháng', 'Hộp, sách, sạc, cáp, tai nghe', 'Máy Mới 100%', 'Dán Màn Hình 4D', 1, 0, '<p>Sản phẩm n&agrave;y ch&uacute;ng t&ocirc;i đang cập nhật nội dung chi tiết, c&aacute;c bạn c&oacute; thể qua trực tiếp cửa h&agrave;ng để xem sản phẩm, v&igrave; h&agrave;ng ch&uacute;ng t&ocirc;i lu&ocirc;n c&oacute; sẵn.</p>\r\n'),
+(9, 2, 'Đề thi toán 9', 'tải xuống.jpg', 10000000, '12 Tháng', 'Hộp, sách, sạc, cáp, tai nghe', 'Máy Mới 100%', 'Dán Màn Hình 4D', 1, 0, '<p>Sản phẩm n&agrave;y ch&uacute;ng t&ocirc;i đang cập nhật nội dung chi tiết, c&aacute;c bạn c&oacute; thể qua trực tiếp cửa h&agrave;ng để xem sản phẩm, v&igrave; h&agrave;ng ch&uacute;ng t&ocirc;i lu&ocirc;n c&oacute; sẵn.</p>\r\n'),
+(10, 2, 'Đề thi toán 8', 'tải xuống.jpg', 10000000, '12 Tháng', 'Hộp, sách, sạc, cáp, tai nghe', 'Máy Mới 100%', 'Dán Màn Hình 4D', 1, 0, '<p>Sản phẩm n&agrave;y ch&uacute;ng t&ocirc;i đang cập nhật nội dung chi tiết, c&aacute;c bạn c&oacute; thể qua trực tiếp cửa h&agrave;ng để xem sản phẩm, v&igrave; h&agrave;ng ch&uacute;ng t&ocirc;i lu&ocirc;n c&oacute; sẵn.</p>\r\n'),
+(11, 4, 'Đề thi toán 8', 'tải xuống.jpg', 10000000, '12 Tháng', 'Hộp, sách, sạc, cáp, tai nghe', 'Máy Mới 100%', 'Dán Màn Hình 4D', 1, 1, 'Sản phẩm này chúng tôi đang cập nhật nội dung chi tiết, các bạn có thể qua trực tiếp cửa hàng để xem sản phẩm, vì hàng chúng tôi luôn có sẵn.'),
+(26, 9, 'Đề thi toán 12', 'tải xuống.jpg', 10000000, '12 Tháng', 'Hộp, sách, sạc, cáp, tai nghe', 'Máy Mới 100%', 'Dán Màn Hình 4D', 1, 1, 'Sản phẩm này chúng tôi đang cập nhật nội dung chi tiết, các bạn có thể qua trực tiếp cửa hàng để xem sản phẩm, vì hàng chúng tôi luôn có sẵn.'),
+(27, 9, 'Đề thi toán 7', 'tải xuống.jpg', 10000000, '12 Tháng', 'Hộp, sách, sạc, cáp, tai nghe', 'Máy Mới 100%', 'Dán Màn Hình 4D', 1, 0, 'Sản phẩm này chúng tôi đang cập nhật nội dung chi tiết, các bạn có thể qua trực tiếp cửa hàng để xem sản phẩm, vì hàng chúng tôi luôn có sẵn.'),
+(28, 9, 'Đề thi ielts', 'tải xuống.jpg', 10000000, '12 Tháng', 'Hộp, sách, sạc, cáp, tai nghe', 'Máy Mới 100%', 'Dán Màn Hình 4D', 1, 0, 'Sản phẩm này chúng tôi đang cập nhật nội dung chi tiết, các bạn có thể qua trực tiếp cửa hàng để xem sản phẩm, vì hàng chúng tôi luôn có sẵn.');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_full` varchar(255) NOT NULL,
+  `user_mail` varchar(255) NOT NULL,
+  `user_pass` varchar(255) NOT NULL,
+  `user_level` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_full`, `user_mail`, `user_pass`, `user_level`) VALUES
+(1, 'Vietpro Academy', 'vietpro.edu.vn@gmail.com', '123456', 1),
+(2, 'Administrator', 'admin@gmail.com', '123456', 1),
+(3, 'Nguyễn Van A', 'nguyenvana@gmail.com', '123456', 2),
+(4, 'Nguyễn Van B', 'nguyenvanb@gmail.com', '123456', 2),
+(5, 'Nguyễn Van C', 'nguyenvanc@gmail.com', '123456', 2),
+(6, 'Nguyễn Van D', 'nguyenvand@gmail.com', '123456', 2);
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cat_id`),
+  ADD UNIQUE KEY `cat_name` (`cat_name`);
+
+--
+-- Chỉ mục cho bảng `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`comm_id`);
+
+--
+-- Chỉ mục cho bảng `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`prd_id`);
+
+--
+-- Chỉ mục cho bảng `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_mail` (`user_mail`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `category`
+--
+ALTER TABLE `category`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT cho bảng `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `comm_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT cho bảng `product`
+--
+ALTER TABLE `product`
+  MODIFY `prd_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT cho bảng `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
