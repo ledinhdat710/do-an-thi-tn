02103ac605474aa20504405f1509aa69d3b3a95f
ledinhdat710 <80235959+ledinhdat710@users.noreply.github.com>
2023-11-16 21:05:12 +0700

    ds hoc sinh

---
+++ app/Http/Controllers/HocSinhController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\HocSinh;
+use DB;
+use Excel;
+use Auth;
+use App\Imports\HocSinhImport;
+
+class HocSinhController extends Controller
+{
+    public function __construct()
+    {
+        if (Auth::check()) {
+            view()->share('user', Auth::user());
+        }
+    }
+
+    public function dashadminn()
+    {
+
+        return view('admin.layout.dashbroad_ad');
+    }
+    public function geths()
+    {
+        $hocsinh = HocSinh::all();
+
+        return view('admin.hocsinh.dshocsinh', ['hocsinh' => $hocsinh]);
+    }
+
+    public function getthemhs()
+    {
+        return view('admin.hocsinh.themhocsinh');
+    }
+
+    public function postthemhs(Request $request)
+    {
+        $this->validate(
+            $request,
+            [
+                'hoten' => 'required|numeric',
+                'hinhanh' => 'required|email|unique:users',
+                'diachi' => 'required|password|min:32',
+                'sdt' => 'required',
+                'id_mh' => 'required'
+            ],
+            [
+                'hoten.required' => 'Bạn chưa nhập họ tên',
+                'hoten.numeric' => 'Tên người dùng ít nhất 3 kí tự',
+                'email.required' => 'Vui lòng nhập email',
+                'email.email' => 'Vui lòng nhập đúng định dạng email',
+                'email.unique' => 'Email đã tồn tại vui lòng nhập emil khác!',
+                'password.required' => 'Vui lòng nhập password',
+                'password.password' => 'Vui lòng nhập đúng định dạng password',
+                'password.min' => 'Password ít nhất 32 ký tự',
+
+            ]
+        );
+        //sau khi bắt lỗi xong, lấy dlieu lưu vào trong model
+        $user = new User;
+        $user->name = $request->tenuser;
+        $user->email = $request->email;
+        $user->password = hash::make($request->password);
+        $user->quyen = $request->quyen;
+        $user->save();
+        //dẫn về trang 
+        return redirect()->back()->with('thongbao', 'Thêm thành công!');
+    }
+    public function imporths()
+    {
+        return view('admin.hocsinh.import');
+    }
+    public function import()
+    {
+        Excel::import(new HocSinhImport(), request()->file('file'));
+
+        return redirect('admin/hocsinh/dshocsinh');
+    }
+}
---
+++ resources/views/admin/hocsinh/dshocsinh.blade.php
@@ -0,0 +1,63 @@
+@extends('admin.layout.dashadmin');
+@section('title','Quản Lý Học Sinh');
+@section('main')
+<div class="design_cauhoi">
+
+    <a href="admin/hocsinh/themhocsinh"><button data-toggle="modal" data-target="#modal_form_horizontal2" type="button" style="background: #213351" class="btn btn-primary">Thêm
+        </button></a>
+    <a href="importhocsinh"> <button type="button" style="background: #213351" class="btn btn-primary">Import</button></a>
+    <a href="export"><button type="button" style="background: #213351" class="btn btn-primary">Export</button></a>
+
+
+
+</div>
+
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h6 class="panel-title"><i class="fas fa-chalkboard-teacher"></i>QUẢN LÝ HỌC SINH</h6>
+    </div>
+    <div class="datatable">
+        @if(session('thongbao'))
+        <div class="alert alert-success">
+            <span class="glyphicon glyphicon-ok icon-oke"></span> {{session('thongbao')}}
+        </div>
+        @endif
+        <table class="table tbl">
+            <thead>
+                <tr>
+                    <th>STT</th>
+                    <th>Họ tên</th>
+                    <th>Hình ảnh</th>
+                    <th>Địa chỉ</th>
+                    <th>Số điện thoại</th>
+                    <th>Sửa</th>
+                    <th>Xóa</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $stt = 1;
+                foreach ($hocsinh as $hs) : $stt ?>
+                    <tr>
+                        <td>{{ $stt++ }}</td>
+                        <td>{{$hs->hoten}}</td>
+                        <td><img src="imgs/user/{{$hs->hinhanh}}" alt="" width="70" height="90"></td>
+                        <td>{{$hs->diachi}}</td>
+                        <td>{{$hs->sdt}}</td>
+
+                        <td><a href="../suahocsinh/{{$hs->id_hs}}">
+                                <button data-toggle="modal" class="btn btnsuach">Sửa</button>
+                            </a>
+                        </td>
+                        <td><a onclick="return confirm('Bạn có chắc chắn muốn xóa không?')" href="xoahocsinh/{{$hs->id_hs}}">
+                                <button type="button" class="btn btnxoach">Xóa</button>
+                            </a>
+                        </td>
+
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+@stop
